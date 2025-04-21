2ae1c398efc663fe73ee6c6b5f3f413379396d65
영광 <dmb07223@gmail.com>
2025-04-21 22:42:50 +0900

    Spring boot create

---
+++ src/main/java/com/project/noto/NotoApplication.java
@@ -0,0 +1,13 @@
+package com.project.noto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/noto/NotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.noto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
